8465b2f3918d723a0e5cd7b6465225ae892209cb
rake_13@naver.com <wltjq1203@icloud.com>
2024-11-13 14:35:32 +1100

    initial init

---
+++ src/main/java/dubaichamber/dreamintership/DreamInterShipApplication.java
@@ -0,0 +1,13 @@
+package dubaichamber.dreamintership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamInterShipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamInterShipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dubaichamber/dreamintership/DreamInterShipApplicationTests.java
@@ -0,0 +1,13 @@
+package dubaichamber.dreamintership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamInterShipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
