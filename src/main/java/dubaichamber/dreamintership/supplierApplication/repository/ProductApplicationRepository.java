6fd7c0a472cc02c04ea5de70987491f6bbfe0875
rake_13@naver.com <wltjq1203@icloud.com>
2024-11-15 16:40:44 +1100

    ProductApplicationRepository가 올바른 Id를 사용하도록 수정

--- src/main/java/dubaichamber/dreamintership/supplierApplication/repository/ProductApplicationRepository.java
+++ src/main/java/dubaichamber/dreamintership/supplierApplication/repository/ProductApplicationRepository.java
@@ -1,7 +1,8 @@
 package dubaichamber.dreamintership.supplierApplication.repository;
 
 import dubaichamber.dreamintership.supplierApplication.entity.ProductApplication;
+import dubaichamber.dreamintership.supplierApplication.entity.ProductApplicationId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductApplicationRepository extends JpaRepository<ProductApplication, Long> {
+public interface ProductApplicationRepository extends JpaRepository<ProductApplication, ProductApplicationId> {
 }
