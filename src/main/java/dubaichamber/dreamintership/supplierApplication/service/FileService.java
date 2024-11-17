66bd7073b12c6b308ab120add43f29950704e422
rake_13@naver.com <wltjq1203@icloud.com>
2024-11-17 21:53:18 +1100

    파일 업로드를 위한 서비스 클래스 작성

---
+++ src/main/java/dubaichamber/dreamintership/supplierApplication/service/FileService.java
@@ -0,0 +1,33 @@
+package dubaichamber.dreamintership.supplierApplication.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class FileService {
+
+    private final Path fileStorageLocation = Paths.get("uploads").toAbsolutePath().normalize();
+
+    public FileService() {
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        try {
+            Path targetLocation = this.fileStorageLocation.resolve(file.getOriginalFilename());
+            Files.copy(file.getInputStream(), targetLocation);
+            return targetLocation.toString();
+        } catch (IOException ex) {
+            throw new RuntimeException("Could not store file " + file.getOriginalFilename() + ". Please try again!", ex);
+        }
+    }
+}
