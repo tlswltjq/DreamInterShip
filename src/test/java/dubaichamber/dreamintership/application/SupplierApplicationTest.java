06e2496959d0e5e614a13f77c644a659d43077c5
rake_13@naver.com <wltjq1203@icloud.com>
2024-11-18 22:30:36 +1100

    supplierForm제출 통합 테스트 작성

---
+++ src/test/java/dubaichamber/dreamintership/application/SupplierApplicationTest.java
@@ -0,0 +1,74 @@
+package dubaichamber.dreamintership.application;
+
+import dubaichamber.dreamintership.supplierApplication.api.ApiController;
+import dubaichamber.dreamintership.supplierApplication.entity.*;
+import dubaichamber.dreamintership.supplierApplication.model.ProductAndDescription;
+import dubaichamber.dreamintership.supplierApplication.model.SupplierForm;
+import dubaichamber.dreamintership.supplierApplication.service.*;
+import dubaichamber.dreamintership.util.FixtureFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+@DisplayName("Application Submitting Test")
+@ExtendWith(MockitoExtension.class)
+public class SupplierApplicationTest {
+    private final FixtureFactory factory = new FixtureFactory();
+
+    @Mock
+    ApplicationService applicationService;
+    @Mock
+    CompanyService companyService;
+    @Mock
+    FileService fileService;
+    @Mock
+    ProductService productService;
+    @Mock
+    ProductApplicationService productApplicationService;
+    @InjectMocks
+    ApiController apiController;
+
+    @DisplayName("Test handleSupplierForm")
+    @Test
+    void testHandleSupplierForm() {
+        String companyName = "new_company";
+        String contactPerson = "Mr.Shin";
+        String email = "wltjq1203@icloud.com";
+        String address = "30 Chuchill Ave";
+        String phoneNumber = "0401377916";
+        String website = "https://www.github.com/tlswltjq";
+        List<ProductAndDescription> products = List.of(new ProductAndDescription("Apple", "sweet"), new ProductAndDescription("lemon", "sour"));
+        MockMultipartFile productCatalogue = new MockMultipartFile("fruit", "Fruits.zip", "application/zip", "sample zip".getBytes());
+        String comment = "our products are good";
+        String fileUrl = "/Users/sinjiseop/Projects/DreamInterShip/uploads/" + productCatalogue.getOriginalFilename();
+
+        SupplierForm supplierForm = factory.createSupplierForm(companyName, contactPerson, email, address, phoneNumber, website, products, productCatalogue, comment);
+        CompanyId companyId = factory.createCompanyId(companyName, contactPerson);
+        Company company = factory.createCompany(companyId);
+        Application application = factory.createApplication(company, email, address, phoneNumber, website, fileUrl, comment);
+
+        when(companyService.createCompany(companyName, contactPerson)).thenReturn(company);
+        when(fileService.storeFile(productCatalogue)).thenReturn(fileUrl);
+        when(applicationService.createApplication(company, email, address, phoneNumber, website, fileUrl, comment)).thenReturn(application);
+        when(productService.createProduct(any(String.class), any(String.class))).thenReturn(mock(Product.class));
+        when(productApplicationService.crateProductApplication(any(Product.class), any(Application.class))).thenReturn(mock(ProductApplication.class));
+
+        apiController.handleFormSubmission(supplierForm);
+
+        verify(companyService).createCompany(companyName, contactPerson);
+        verify(fileService).storeFile(productCatalogue);
+        verify(applicationService).createApplication(company, email, address, phoneNumber, website, fileUrl, comment);
+        verify(productService, times(products.size())).createProduct(any(String.class), any(String.class));
+        verify(productApplicationService, times(products.size())).crateProductApplication(any(Product.class), any(Application.class));
+    }
+}
--- src/test/java/dubaichamber/dreamintership/util/FixtureFactory.java
+++ src/test/java/dubaichamber/dreamintership/util/FixtureFactory.java
@@ -1,8 +1,12 @@
 package dubaichamber.dreamintership.util;
 
 import dubaichamber.dreamintership.supplierApplication.entity.*;
+import dubaichamber.dreamintership.supplierApplication.model.ProductAndDescription;
+import dubaichamber.dreamintership.supplierApplication.model.SupplierForm;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class FixtureFactory {
     public Product createProduct(String productName, String description) {
@@ -23,7 +27,7 @@ public class FixtureFactory {
                 .build();
     }
 
-    public Application createApplication(Company company,  String email, String address, String phoneNumber, String webSite, String fileUrl, String comment) {
+    public Application createApplication(Company company, String email, String address, String phoneNumber, String webSite, String fileUrl, String comment) {
         return Application.builder()
                 .applicationId(1L)
                 .company(company)
@@ -37,10 +41,24 @@ public class FixtureFactory {
                 .build();
     }
 
-    public ProductApplication createProductApplication(Product product, Application application){
+    public ProductApplication createProductApplication(Product product, Application application) {
         return ProductApplication.builder()
                 .productName(product)
                 .applicationId(application)
                 .build();
+    }
+
+    public SupplierForm createSupplierForm(String companyName, String contactPerson, String email, String address, String phoneNumber, String website, List<ProductAndDescription> products , MultipartFile productCatalogue, String comment) {
+        SupplierForm supplierForm = new SupplierForm();
+        supplierForm.setCompanyName(companyName);
+        supplierForm.setContactPerson(contactPerson);
+        supplierForm.setEmail(email);
+        supplierForm.setAddress(address);
+        supplierForm.setPhoneNumber(phoneNumber);
+        supplierForm.setWebsite(website);
+        supplierForm.setProducts(products);
+        supplierForm.setProductCatalogue(productCatalogue);
+        supplierForm.setComment(comment);
+        return supplierForm;
     }
 }
